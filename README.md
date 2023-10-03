b70c49eab7d522ef92143385143468ef180ffe7a
Steffen Busch <steffen.busch@gmail.com>
2023-10-03 19:35:10 +0200

    Initial version

--- README.md
+++ README.md
@@ -1,2 +1,88 @@
-# http-bad-request-tester
-Simple HTTP Bad Request Testing Utility
+# Simple HTTP Bad Request Testing Utility
+
+## Description
+
+This utility is a Bash script designed to automate the process of testing HTTP bad requests against a given host. It's useful for quickly performing a series of tests based on predefined `.txt` test case files.
+
+## Features
+
+- Easily specify the host to test against
+- Specify custom port (default is 80)
+- Supports wildcards for specifying multiple test cases
+- Color-coded output for better readability
+- Supports disabling color output
+
+## Requirements
+
+- Bash 4.0 or later
+- Ncat version of `nc` from the Nmap project
+
+## Installation
+
+1. Clone this repository:
+
+    ```bash
+    git clone https://github.com/steffenbusch/http-bad-request-tester
+    ```
+
+2. Navigate into the directory:
+
+    ```bash
+    cd http-bad-request-tester
+    ```
+
+3. Make the script executable:
+
+    ```bash
+    chmod +x run.sh
+    ```
+
+## Usage
+
+### Basic Usage
+
+To test against a specific host:
+
+```bash
+./run.sh --host arm.stbu.net
+```
+
+To test against a specific host and port:
+
+```bash
+./run.sh --host arm.stbu.net --port 8080
+```
+
+To specify a single test case:
+
+```bash
+./run.sh --host arm.stbu.net --testcases good-request.txt
+```
+
+To specify multiple test cases using wildcards:
+
+```bash
+./run.sh --host arm.stbu.net --testcases "http0.9*"
+```
+
+## Help
+
+For the usage guide:
+
+```bash
+./run.sh --help
+```
+
+## Disabling Color Output
+
+To disable color output, set the NO_COLOR environment variable:
+
+```bash
+NO_COLOR=true ./run.sh --host arm.stbu.net
+```
+
+## Disclaimer
+
+This utility is intended for educational and ethical testing purposes only. **Only run it against hosts you own or have explicit permission to test.** Unauthorized testing is illegal and unethical.
+
+Use at your own risk. The author of this utility is not responsible for any illegal activities or misuse.
---
+++ run.sh
@@ -0,0 +1,110 @@
+#!/usr/bin/env bash
+
+# Display usage guide
+usage() {
+  echo "Usage: $0 [OPTIONS]"
+  echo "Options:"
+  echo "  --host HOST       Specify the host to test against."
+  echo "  --port PORT       Specify the port to use. Default is 80."
+  echo "  --testcases FILE  Specify the .txt file(s) for test cases. Supports wildcards."
+  echo "  -h, --help        Show this help message."
+  exit 1
+}
+
+# Initialize variables
+host=""
+port=80
+testcases="*.txt"
+
+# Parse command-line options
+while [ "$#" -gt 0 ]; do
+  case "$1" in
+    --host)
+      host="$2"
+      shift 2
+      ;;
+    --port)
+      port="$2"
+      shift 2
+      ;;
+    --testcases)
+      testcases="$2"
+      shift 2
+      ;;
+    -h|--help)
+      usage
+      ;;
+    *)
+      printf "Unknown parameter: %s\n" "$1"
+      exit 1
+      ;;
+  esac
+done
+
+
+# ANSI color codes
+if [ -z "$NO_COLOR" ] && [ -t 1 ]; then
+    red='\033[0;31m'
+    bold='\033[1m'
+    reset='\033[0m'
+else
+    red=''
+    bold=''
+    reset=''
+fi
+
+# Check if nc is available
+if ! command -v nc >/dev/null 2>&1; then
+  echo "Error: nc command is not available."
+  exit 1
+fi
+
+# Check if it is from the Nmap project
+if ! nc --version 2>&1 | grep -iq 'nmap'; then
+  echo "Error: This script requires the Ncat version of nc from the Nmap project."
+  exit 1
+fi
+
+# Check if host is provided
+if [ -z "$host" ]; then
+    printf "Please provide a host using --host.\n"
+    exit 1
+fi
+
+# Counter for processed files
+counter=1
+
+# Calculate the total number of files to be processed
+total_files=$(find . -maxdepth 1 -name "$testcases" -type f | wc -l)
+
+if [ "$total_files" -eq 0 ]; then
+    printf "No matching files found for testcases: %s\n" "$testcases"
+    exit 1
+fi
+
+# Loop through all matching files sorted alphabetically
+for file in $(find . -maxdepth 1 -name "$testcases" -type f | sort); do
+    file=${file#./}  # remove leading './' returned by find
+
+    # Replace 'example.com' with the provided host and store it in a variable
+    modified_content=$(sed "s/example.com/$host/g" "$file")
+
+    # Print the name and modified content of the file
+    printf -- "${bold}======= Sending File: ${red}%s${reset}${bold} [Test %d/%d] =======${reset}\n\n" "$file" "$counter" "$total_files"
+    printf -- "${bold}--- Request (Modified Content of the File) ---${reset}\n"
+    printf -- "%s\n\n" "$modified_content"
+
+    # Use a separator to delineate Request from Response
+    printf -- "${bold}--- Server Response ---${reset}\n"
+
+    # Send the modified content using nc and capture the server's response
+    printf -- "%s" "$modified_content" | nc $host $port
+
+    # Print a separator line
+#    printf "\n============================================================\n\n"
+    printf -- "\n\n"
+
+    # Increment the counter
+    ((counter++))
+done
+
