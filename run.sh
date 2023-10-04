546f9a51594c48b08e9002101c20e181aeca1908
Steffen Busch <steffen.busch@gmail.com>
2023-10-04 20:24:59 +0200

    Fix indentation

--- run.sh
+++ run.sh
@@ -53,16 +53,15 @@ while [ "$#" -gt 0 ]; do
   esac
 done
 
-
 # ANSI color codes
 if [ -z "$NO_COLOR" ] && [ -t 1 ]; then
-    red='\033[0;31m'
-    bold='\033[1m'
-    reset='\033[0m'
+  red='\033[0;31m'
+  bold='\033[1m'
+  reset='\033[0m'
 else
-    red=''
-    bold=''
-    reset=''
+  red=''
+  bold=''
+  reset=''
 fi
 
 # Check if nc is available
@@ -79,8 +78,8 @@ fi
 
 # Check if host is provided
 if [ -z "$host" ]; then
-    printf "Please provide a host using --host.\n"
-    exit 1
+  printf "Please provide a host using --host.\n"
+  exit 1
 fi
 
 # Report file and temporary response output file creation when --overview was specified
@@ -97,53 +96,52 @@ counter=1
 total_files=$(find . -maxdepth 1 -name "$testcases" -type f | wc -l)
 
 if [ "$total_files" -eq 0 ]; then
-    printf "No matching files found for testcases: %s\n" "$testcases"
-    exit 1
+  printf "No matching files found for testcases: %s\n" "$testcases"
+  exit 1
 fi
 
 # Loop through all matching files sorted alphabetically
 for file in $(find . -maxdepth 1 -name "$testcases" -type f | sort); do
-    file=${file#./}  # remove leading './' returned by find
+  file=${file#./}  # remove leading './' returned by find
 
-    # Replace 'example.com' with the provided host and store it in a variable
-    # Append a marker '@@@' to ensure trailing newlines are included
-    modified_content=$(sed "s/example.com/$host/g" "$file"; echo '@@@')
+  # Replace 'example.com' with the provided host and store it in a variable
+  # Append a marker '@@@' to ensure trailing newlines are included
+  modified_content=$(sed "s/example.com/$host/g" "$file"; echo '@@@')
 
-    # Remove the marker '@@@' before sending the request
-    modified_content=${modified_content%@@@}
+  # Remove the marker '@@@' before sending the request
+  modified_content=${modified_content%@@@}
 
-    # Print the name and modified content of the file
-    printf -- "${bold}======= Sending File: ${red}%s${reset}${bold} [Test %d/%d] =======${reset}\n\n" "$file" "$counter" "$total_files"
-    printf -- "${bold}--- Request (Modified Content of the File) ---${reset}\n"
-    if [ ${#modified_content} -gt 250 ]; then
-      printf -- "%s...(%d more characters)\n\n" "${modified_content:0:250}" $((${#modified_content}-250))
-    else
-      printf -- "%s\n\n" "$modified_content"
-    fi
+  # Print the name and modified content of the file
+  printf -- "${bold}======= Sending File: ${red}%s${reset}${bold} [Test %d/%d] =======${reset}\n\n" "$file" "$counter" "$total_files"
+  printf -- "${bold}--- Request (Modified Content of the File) ---${reset}\n"
+  if [ ${#modified_content} -gt 250 ]; then
+    printf -- "%s...(%d more characters)\n\n" "${modified_content:0:250}" $((${#modified_content}-250))
+  else
+    printf -- "%s\n\n" "$modified_content"
+  fi
 
-    # Use a separator to delineate Request from Response
-    printf -- "${bold}--- Server Response ---${reset}\n"
+  # Use a separator to delineate Request from Response
+  printf -- "${bold}--- Server Response ---${reset}\n"
 
-    # Send the modified content using nc, capture the server's response,
-    # and save it temporarily for extracting HTTP status
-        printf -- "%s" "$modified_content" | nc "${ip:-$host}" "$port" | tee "$temp_reponse_out"
+  # Send the modified content using nc, capture the server's response,
+  # and save it temporarily for extracting HTTP status
+  printf -- "%s" "$modified_content" | nc "${ip:-$host}" "$port" | tee "$temp_reponse_out"
 
-    # Extract and store HTTP status and filename for the overview
-    http_status=$(head -n 1 "$temp_reponse_out" | awk '{print $2}')
-    printf "%2d %-36s %s\n" "$counter" "$file" "$http_status" >> "$overview_file"
+  # Extract and store HTTP status and filename for the overview
+  http_status=$(head -n 1 "$temp_reponse_out" | awk '{print $2}')
+  printf "%2d %-36s %s\n" "$counter" "$file" "$http_status" >> "$overview_file"
 
+  # Print a separator line
+  printf -- "\n\n"
 
-    # Print a separator line
-    printf -- "\n\n"
-
-    # Increment the counter
-    ((counter++))
+  # Increment the counter
+  ((counter++))
 done
 
 # Display overview if --overview was specified
 if [ "$overview_report" = true ]; then
-    echo -e "\n--- Overview ---"
-    while IFS= read -r line; do
-        echo "$line"
-    done < "$overview_file"
+  echo -e "\n--- Overview ---"
+  while IFS= read -r line; do
+    echo "$line"
+  done < "$overview_file"
 fi
